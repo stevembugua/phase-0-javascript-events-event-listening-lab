ff7ed98cbf87a119be9f2564623d3937d63a7a75
mbugua <mbuguafaimba@gmail.com>
2022-11-17 18:10:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+
+
+
+function addingEventListener (callBack){
+      input.addEventListener('click', callBack);
 }
+
+
+
+addingEventListener(clickAlert)
